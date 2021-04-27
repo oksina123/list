3e00c29c0537a9dbceaf65d8e26a789e7b8f19c0
aleksander oksinski <aleksanderoksinski@gmail.com>
2021-04-27 19:32:51 +0200

    added test of remove function

--- test.cpp
+++ test.cpp
@@ -11,14 +11,18 @@ int main(int argc, char* argv[]) {
     lista.push(37109);
     lista.push(209421);
     try {
-        cout << lista.get(0) << " " << lista.get(1) << " "<< lista.get(2);
+        cout << lista.get(0) << " " << lista.get(1) << " "<< lista.get(2)<<std::endl;
     } catch (std::exception e) {
         cout << "zly indeks";
     }
+    lista.remove(209421);
 
+    std::cout<<"lista.contains(209421)->"<<(lista.contains(209421)? "true" : "false")<< std::endl;
     std::cout<< "lista.contains(123518) -> " << (lista.contains(123518) ? "true" : "false") << std::endl;
     std::cout<< "lista.contains(4) ->" << (lista.contains(4) ?  "true":"false")<<std::endl;
     std::cout<<lista.size();
+
+
 
  // a ? b : c
  // a=true -> b
